bdb02b4afea7ef1377dccd46ae4365e2c815b5ab
Jose E. Martinez <30488583+je-martinez@users.noreply.github.com>
2023-05-05 21:56:42 -0600

    chore: add cypress without coverage

---
+++ cypress.config.js
@@ -0,0 +1,23 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
+    baseUrl: "http://localhost:8080",
+    setupNodeEvents(on, config) {
+      require("@cypress/code-coverage/task")(on, config);
+      // include any other plugin code...
+
+      // It's IMPORTANT to return the config object
+      // with any changed environment variables
+      return config;
+    },
+  },
+  component: {
+    specPattern: "src/**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}",
+    devServer: {
+      framework: "vue",
+      bundler: "vite",
+    },
+  },
+});
